3da9bcdf4a328e57c8cf26c3dcc574183a9c98e4
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2024-04-29 00:12:01 +0900

    feat: Add Slack app token to environment variables

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import { App } from "@slack/bolt";
+import { env } from "@/env";
+
+// ボットトークンと Signing Secret を使ってアプリを初期化します
+const app = new App({
+  token: env.SLACK_BOT_TOKEN,
+  signingSecret: env.SLACK_SIGNING_SECRET,
+  appToken: env.SLACK_APP_TOKEN,
+  port: Number(env.PORT || 3000),
+  socketMode: true,
+});
+
+// Listens to incoming messages that contain "hello"
+app.message("hello", async ({ message, say }: any) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
+
+app.message("", async ({ message, say }) => {
+  if (!message.subtype) {
+    await say(`Hello, <@${message.user}>. You said: ${message.text}`);
+  }
+});
+
+(async () => {
+  // Start your app
+  await app.start();
+  console.log("⚡️ Bolt app is running!");
+})();
