bb8024b1585b2d54635d85f80e053a5b137515fd
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2024-05-10 16:50:15 +0900

    refactor: Update command registration with prefixCommand function

--- src/listeners/commands/index.ts
+++ src/listeners/commands/index.ts
@@ -1,10 +1,15 @@
 import { App } from "@slack/bolt";
 import testCallback from "./test";
 import imagineCallback from "./imagine";
+import { debug } from "../../constants";
+
+function prefixCommand(command: string): string {
+  return debug ? `${command}-dev` : command;
+}
 
 function register(app: App) {
-  app.command("/test", testCallback);
-  app.command("/imagine", imagineCallback);
+  app.command(prefixCommand("/test"), testCallback);
+  app.command(prefixCommand("/imagine"), imagineCallback);
 }
 
 export default { register };
